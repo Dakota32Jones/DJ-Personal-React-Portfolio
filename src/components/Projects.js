69208caa9309007994c5a77a483a697335208f44
Dakota Jones <dakota32jones@gmail.com>
2022-11-06 01:23:02 -0500

    added pills for the project need to add tab panes tomorrow

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,53 @@
+import { Nav, Tab } from "react-bootstrap";
+
 export const Projects = () => {
-  return {};
+  const projects = [
+    {
+      title: "Project 1",
+      description:
+        "Front End application that fetches data from multiple API's.",
+      imgUrl: projImg,
+      githubRepoUrl: "",
+    },
+    {
+      title: "Project 2",
+      description:
+        "Front End application that fetches data from multiple API's.",
+      imgUrl: projImg2,
+      githubRepoUrl: "",
+    },
+    {
+      title: "Project 3",
+      description:
+        "Front End application that fetches data from multiple API's.",
+      imgUrl: projImg3,
+      githubRepoUrl: "",
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>Lorem Ipsum</p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey={first}>Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey={second}>Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey={third}>Tab Three</Nav.Link>
+                </Nav.Item>
+                {/* need to add tab pane tomorrow for projects */}
+              </Nav>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
