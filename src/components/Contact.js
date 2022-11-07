9cb8f6a2ad152f90568274cbee0627bc81a0d29d
Dakota Jones <dakota32jones@gmail.com>
2022-11-06 18:25:55 -0600

    added contact section to page with first initial state

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+export const Contact = () => {
+    
+const formInitialDetails = {
+    firstName: '',
+    lasName: '',
+    email: '',
+    phone: '',
+    message: ''
+}
+return ( 
+
+)
+}
