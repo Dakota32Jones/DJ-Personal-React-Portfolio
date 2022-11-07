135bc829fa6f3d28d57caaddfa091b33fc34dbf1
Dakota Jones <dakota32jones@gmail.com>
2022-11-06 18:41:10 -0600

    added contact image and started pre state for input fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import contactImg from "../assets/images/contact-img.png";
 export const Contact = () => {
-    
-const formInitialDetails = {
-    firstName: '',
-    lasName: '',
-    email: '',
-    phone: '',
-    message: ''
-}
-return ( 
+  // initial state object
+  const formInitialDetails = {
+    firstName: "",
+    lasName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+  // creating form details state
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  // submission button update to sending
+  const [buttonText, setButtonText] = useState("Send");
+  // update api call with an error if there's an error
+  const [status, setStatus] = useState({});
 
-)
-}
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="Contact Me" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
